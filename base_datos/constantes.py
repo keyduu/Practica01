0195dd383531fce83ae069e71b86001ea9c93452
keyduu <keyduu@gmail.com>
2020-04-08 17:29:30 +0200

    Las operaciones en base de datos devuelven objetos Movil, se añade el borrado de elementos, solo dalta la modificación de datos.

--- base_datos/constantes.py
+++ base_datos/constantes.py
@@ -1,3 +1,5 @@
-SQL_INSERCION = "INSERT INTO tabla_moviles(marca, modelo, sistema_operativo, memoria, precio)" \
-                " VALUES (%s, %s, %s, %s, %s);"
+SQL_INSERCION = "INSERT INTO tabla_moviles(marca, modelo, sistema_operativo, memoria, precio) " \
+                "VALUES (%s, %s, %s, %s, %s);"
 SQL_CONSULTA = "SELECT * FROM tabla_moviles;"
+SQL_BORRAR = "DELETE FROM tabla_moviles WHERE id = %s;"
+SQL_CONSULTA_ELEMENTO = "SELECT * FROM tabla_moviles WHERE id = %s;"
